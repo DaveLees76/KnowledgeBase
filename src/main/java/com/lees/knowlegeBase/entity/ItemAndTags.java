6fb6544cf0b4f9c0fa83cd833a4eb9bc3a1a26d4
dlees <dlees@ebsco.com>
2018-06-05 10:22:21 -0400

    Find by tag working2!

---
+++ src/main/java/com/lees/knowlegeBase/entity/ItemAndTags.java
@@ -0,0 +1,32 @@
+package com.lees.knowlegeBase.entity;
+
+public class ItemAndTags {
+
+	private String itemTitle;
+	private String itemContent;
+	private String tags;
+	
+	public void setItemTitle(String itemTitle) {
+		this.itemTitle = itemTitle;
+	}
+	
+	public String getItemTitle() {
+		return itemTitle;
+	}
+	
+	public void setItemContent(String itemComtent) {
+		this.itemContent = itemComtent;
+	}
+	
+	public String getItemContent() {
+		return itemContent;
+	}
+	
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+	
+	public String getTags() {
+		return tags;
+	}
+}
---
+++ src/main/java/com/lees/knowlegeBase/entity/ItemResponse.java
@@ -0,0 +1,29 @@
+package com.lees.knowlegeBase.entity;
+
+public class ItemResponse {
+
+	private String title;
+	private String content;
+	
+	public ItemResponse(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+}
