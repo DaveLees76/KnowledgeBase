31382474c63ba7ea67b05fc49d8a7e8975eb9b18
dlees <dlees@ebsco.com>
2018-07-01 13:27:17 -0400

    got search results to display

--- src/main/java/com/lees/knowlegeBase/controller/KnowledgeController.java
+++ src/main/java/com/lees/knowlegeBase/controller/KnowledgeController.java
@@ -6,11 +6,14 @@ import java.util.HashSet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.lees.knowlegeBase.entity.ItemAndTags;
 import com.lees.knowlegeBase.entity.ItemResponse;
 import com.lees.knowlegeBase.manager.IKnowledgeManager;
@@ -40,9 +43,9 @@ public class KnowledgeController {
 		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 	
-	@RequestMapping(value = "/getitemsbytag", method = RequestMethod.GET)
+	@RequestMapping(value = "/getitemsbytag", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ArrayList<ItemResponse> getItemsByTag(String tag) {
-		
+
 		return knowledgeManager.getItemsByTag(tag);
 	}
 }
